85df79337530523128cdca23c3487c9bc9b9c557
rocky_sdn <rockysmith@sending.network>
2023-06-27 17:28:20 +0800

    fix: examples/web

--- examples/web/src/index.js
+++ examples/web/src/index.js
@@ -1,51 +1,49 @@
-import { randomBytes } from "crypto-browserify";
-import { Wallet }  from "ethers";
 import sdk from "sendingnetwork-js-sdk";
 
-function register_in_browser(){
-  window.randomBytes = randomBytes;
-  window.Wallet  = Wallet;
-}
+const baseUrl = 'https://portal0101.sending.network';
+
+const user_id = localStorage.getItem("sdn_user_id");
+const access_token = localStorage.getItem("sdn_access_token");
+const _client = sdk.createClient({
+  baseUrl,
+  userId: user_id ? user_id : undefined,
+  accessToken: access_token ? access_token : undefined
+})
+
 
 function init() {
-  register_in_browser();
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
-        console.log('SW registered: ', registration);
-      }).catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-    });
-  }
-  document.getElementById("test").onclick = hello;
+  // if ('serviceWorker' in navigator) {
+  //   window.addEventListener('load', () => {
+  //     navigator.serviceWorker.register('/sw.js').then(registration => {
+  //       console.log('SW registered: ', registration);
+  //     }).catch(registrationError => {
+  //       console.log('SW registration failed: ', registrationError);
+  //     });
+  //   });
+  // }
+  document.getElementById("test").onclick = testReq;
   document.getElementById("login").onclick = login;
-  document.getElementById("backup").onclick = backup;
   document.getElementById("createRoom").onclick = createRoom;
+}
+
+function showJson(json, elememt) {
+  if (elememt == null){
+    elememt = "resp"
+  }
+  document.getElementById(elememt).innerHTML = JSON.stringify(json, undefined, 2);
 }
 
 export async function createRoom() {
   const loginStr = document.getElementById("resp").innerHTML
-  if (window.loginResult == null
-      || window.loginResult.accessToken == null
-      || window.loginResult.accessToken.length ===0){
-    showJson({"error": "please login first"}, "room_info");
+  if (!localStorage.getItem("sdn_user_id") || !localStorage.getItem("sdn_access_token")) {
+    showJson({"error": "please login first"}, "room_info")
   }
-
   const roomName =  document.getElementById("roomName").value;
   if (roomName == null || roomName.length === 0 ){
     showJson({"error": "please input room name"}, "room_info");
     return;
   }
-
-  console.log(window.loginResult);
-  const client = sdk.createClient({
-    baseUrl: "http://localhost",
-    userId: window.loginResult.user_id,
-    accessToken: window.loginResult.access_token,
-  });
-
-  client.createRoom({
+  _client.createRoom({
     name: roomName,
     visibility: "public",
   }).then((room_info) => {
@@ -57,215 +55,54 @@ export async function createRoom() {
   });
 }
 
-
-export async function hello() {
+export async function testReq() {
   const url = document.querySelector("#url").value
   const res = await fetch(url)
   const json = await res.json();
   showJson(json);
 }
 
-function showJson(json, elememt) {
-  if (elememt == null){
-    elememt = "resp"
-  }
-  document.getElementById(elememt).innerHTML = JSON.stringify(json, undefined, 2);
-}
-
-async function loginViaDID(params) {
-  const prepared = {
-    ...params,
-    nonce: randomBytes(32).toString("hex"),
-    timestamp: new Date().getTime(),
-    token: "",
-  };
-  const message = [
-    prepared.salt,
-    prepared.publicKey,
-    prepared.nonce,
-    prepared.timestamp,
-  ].join("#");
-  try {
-    const from = prepared.publicKey;
-    const msg = `0x${Buffer.from(message, "utf8").toString("hex")}`;
-    let sign = await window.ethereum.request({
-      method: "personal_sign",
-      params: [msg, from, ""],
-    });
-    if (sign.startsWith("0x") || sign.startsWith("0X")) {
-      sign = sign.substring(2);
-      prepared.token = Buffer.from(sign, "hex")
-          .toString("base64")
-          .replace(/\+/g, "-") // Convert '+' to '-'
-          .replace(/\//g, "_"); // Convert '/' to '_'
-    }
-  } catch (err) {
-    console.error(err);
-  }
-  const identifier = {
-    ...prepared,
-    type: "m.id.thirdparty",
-    medium: "did",
-    publicKey: params.publicKey,
-  };
-
-  const loginParams = {
-    identifier,
-    initial_device_display_name: "wasm test",
-    type: "m.login.did.identity",
-    device_id: "wasm_demo",
-  };
-  const response = await fetch("http://localhost/_api/client/v3/login", {
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(loginParams)
-  });
-  const json = await response.json();
-  console.log(json);
-  return json;
-}
-
 export async function login() {
-  const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts",
-  });
-  const account = accounts[0];
-  console.log("select account: " + account);
-  const result = await loginViaDID({
-    publicKey: account,
-    salt: "hiseas_account",
-    address: `did:eth:mainnet:${account}_did`,
-  })
-  window.loginResult = result;
-  showJson(result);
-}
+  if (window.ethereum) {
+    try {
+      const prefix = "did:pkh:eip155:1:";
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const [address] = accounts;
+      let { data: [did] } = await _client.getDIDList(address);
+      const preloginParams = did ? { did } : { address: `${prefix}${address}` }
+      const { message: lMessage, updated, random_server } = await _client.preDiDLogin1(preloginParams);
+      let sign = await window.ethereum.request({
+        method: "personal_sign",
+        params: [lMessage, address, ""],
+      });
+      let identifier = {
+        did,
+        address: did || `${prefix}${address}`,
+        token: sign,
+        message: lMessage
+      };
+      const deviceId = localStorage.getItem("mx_device_id") || null;
+      let loginParams = {
+        type: "m.login.did.identity",
+        updated,
+        identifier,
+        random_server,
+        device_id: deviceId,
+        initial_device_display_name: this.defaultDeviceDisplayName,
+      };
+      const result = await _client.DIDLogin(loginParams);
+      const { access_token, user_id } = result;
 
-async function sendTransaction(transactions) {
-  if (transactions) {
-    if (transactions.length === 0) {
-      alert("Backup successfully");
-    } else {
-      const [transaction] = transactions;
-      const { chainId } = transaction;
-
-      if (window.ethereum.networkVersion === chainId) {
-        window.ethereum
-            .request({
-              method: "eth_sendTransaction",
-              params: transactions,
-            })
-            .then((result) => {
-              console.log("eth_sendTransaction", result);
-            })
-            .catch((error) => {
-              console.log("eth_sendTransaction", error);
-            });
-      } else {
-        try {
-          await window.ethereum
-              .request({
-                method: "wallet_switchEthereumChain",
-                params: [
-                  {
-                    chainId: `0x${Number(chainId).toString(
-                        16
-                    )}`,
-                  },
-                ],
-              })
-              .then(() => {
-                window.ethereum.request({
-                  method: "eth_sendTransaction",
-                  params: transactions,
-                });
-              });
-        } catch (switchError) {
-          console.log("wallet_addEthereumChain");
-          // This error code indicates that the chain has not been added to MetaMask.
-          if (switchError.code === 4902) {
-            try {
-              const networkConf = {
-                "137": {
-                  chainName: "Polygon Mainnet",
-                  rpcUrls: ["https://polygon-rpc.com/"],
-                  nativeCurrency: {
-                    name: "MATIC",
-                    symbol: "MATIC",
-                    decimals: 18,
-                  },
-                  blockExplorerUrls: [
-                    "https://polygonscan.com",
-                  ],
-                },
-              };
-
-              const {
-                chainName,
-                rpcUrls,
-                nativeCurrency,
-                blockExplorerUrls,
-              } = networkConf[chainId];
-              await window.ethereum
-                  .request({
-                    method: "wallet_addEthereumChain",
-                    params: [
-                      {
-                        chainId: `0x${Number(
-                            chainId
-                        ).toString(16)}`,
-                        chainName,
-                        rpcUrls,
-                        nativeCurrency,
-                        blockExplorerUrls,
-                      },
-                    ],
-                  })
-                  .then(() => {
-                    window.ethereum
-                        .request({
-                          method: "wallet_switchEthereumChain",
-                          params: [
-                            {
-                              chainId: `0x${Number(
-                                  chainId
-                              ).toString(16)}`,
-                            },
-                          ],
-                        })
-                        .then(() => {
-                          window.ethereum.request({
-                            method: "eth_sendTransaction",
-                            params: transactions,
-                          });
-                        });
-                  });
-            } catch (addError) {
-              // handle "add" error
-            }
-          }
-          // handle other "switch" errors
-        }
-      }
+      localStorage.setItem("sdn_access_token", access_token);
+      localStorage.setItem("sdn_user_id", user_id);
+      localStorage.setItem("sdn_user_address", address);
+      showJson(result);
+    } catch (error) {
+      showJson(error);
     }
   }
-}
-
-export async function backup() {
-  if (typeof window.loginResult == "undefined") {
-    return;
-  }
-  const response = await fetch("http://localhost/_api/client/unstable/social_graph/backup_transaction", {
-    method: "GET",
-    headers: {
-      'Authorization': 'Bearer ' + window.loginResult.access_token
-    },
-  });
-  const json = await response.json();
-  console.log(json);
-  showJson(json);
-  await sendTransaction(json["transactions"]);
 }
 
 init();
